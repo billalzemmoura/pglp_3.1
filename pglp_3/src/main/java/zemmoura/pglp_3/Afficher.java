978273224ea677062ba2da9a5703becde46891f2
billal zemmoura <billal.zemmoura@ens.uvsq.fr>
2020-03-05 19:19:32 +0100

    création de la classe Afficher

---
+++ pglp_3/src/main/java/zemmoura/pglp_3/Afficher.java
@@ -0,0 +1,13 @@
+package zemmoura.pglp_3;
+
+public class Afficher {
+	public void   AfficherCoordone(String message) {
+		System.out.println(message);
+	    
+		}	
+		public void   AfficherSalaire(double salaire) {
+		System.out.println(salaire);
+	    
+		}	
+
+}
