a70170788c0c9e5be1c53cba21e134fe3512d664
billal zemmoura <billal.zemmoura@ens.uvsq.fr>
2020-03-05 20:00:07 +0100

    ajout des méthodes d'affichage dans la class Employe

--- pglp_3/src/main/java/zemmoura/pglp_3/Employe.java
+++ pglp_3/src/main/java/zemmoura/pglp_3/Employe.java
@@ -3,18 +3,28 @@ package zemmoura.pglp_3;
 
 
 public class Employe {
-private final String nom;
-private final String adresse;
-private double salaire;
 	
-	public Employe(double salaire,String nom,String adresse) {
-		this.salaire=salaire;
-		this.nom=nom;
-		this.adresse=adresse;
-		
+	
+	private final String nom;
+	private final String adresse;
+	private double salaire;
 
-        } 
-        public double calculSalaire() {return salaire*12;}
-        
+	public Employe(double salaire, String nom, String adresse) {
+		this.salaire = salaire;
+		this.nom = nom;
+		this.adresse = adresse;
+	}
+
+	public double calculSalaire() {
+		return salaire * 12;
+	}
+
+	public void AfficheCordonnes() {
+		Instance.printer.AfficherCoordone(nom + "," + adresse);
+	}
+
+	public void AfficheSalaire() {
+		Instance.printer.AfficherSalaire(calculSalaire());
+	}
 
 }
